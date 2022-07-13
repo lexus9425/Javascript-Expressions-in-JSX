c1bd1bc46af6b855667dcc4aaf33ea64188cc82a
lexus9425 <68475116+lexus9425@users.noreply.github.com>
2022-07-13 12:52:30 -0400

    Update index.js - Added third var for last name - Changed name var to first name var - Added both first and last name var to "Hello" h1

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const name = "Alexis";
-
+const fName = "Alexis";
+const lName = "Ojeda";
 const luckyNum = 12;
 
 ReactDOM.render(
   <div>
-    <h1>Hello {name}</h1>
+    <h1>
+      Hello {fName} {lName}!
+    </h1>
     <p>Your lucky number is {luckyNum}</p>
   </div>,
   document.getElementById("root")
