7dce8844b6211e63e7145934fd3777e3a1666c5d
lexus9425 <68475116+lexus9425@users.noreply.github.com>
2022-07-13 12:45:13 -0400

    Update index.js - add two vars - add ReactDOM.render - Render h1 with value of first var and p with value of second var

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const name = "Alexis";
+
+const luckyNum = 12;
+
+ReactDOM.render(
+  <div>
+    <h1>Hello {name}</h1>
+    <p>Your lucky number is {luckyNum}</p>
+  </div>,
+  document.getElementById("root")
+);
